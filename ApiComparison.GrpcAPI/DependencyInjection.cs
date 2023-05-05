97bfe14485e10c9628254a255b9bd0d5dec95aaf
Andrei Costache <costacheandrei43@gmail.com>
2023-05-05 17:29:53 +0300

    partially working grpc services

---
+++ ApiComparison.GrpcAPI/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using ApiComparison.Application.Interfaces;
+using ApiComparison.Infrastructure.BusinessLogicServices;
+
+namespace ApiComparison.GrpcApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServiceLayer(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IAccountService, AccountService>();
+        serviceCollection.AddScoped<IAddressService, AddressService>();
+        serviceCollection.AddScoped<IDishService, DishService>();
+        serviceCollection.AddScoped<IIngredientService, IngredientService>();
+        serviceCollection.AddScoped<IUserService, UserService>();
+
+        return serviceCollection;
+    }
+}
--- ApiComparison.GrpcAPI/Program.cs
+++ ApiComparison.GrpcAPI/Program.cs
@@ -1,12 +1,19 @@
+using ApiComparison.GrpcApi.Services;
+using ApiComparison.Infrastructure;
+using Grpc.Net.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
+builder.Services.AddInfrastructureLayer(builder.Configuration);
 // Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+app.MapGrpcService<AccountService>();
 
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
